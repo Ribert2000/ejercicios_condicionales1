bb04ec7e329577aaa706d110afac11b4b07e1630
Ribert2000 <ribert_rodriguez21@hotmail.com>
2021-09-13 12:15:54 -0500

    11

--- condicioanles_.py
+++ condicioanles_.py
@@ -130,3 +130,15 @@ elif (num1 > num2):
 elif (num1 < num2):
     suma = (num1  + num2)
     print(f"Como el primer numero es menor al segundo, se suman, el resultado es: {suma}")
+    
+#ej10
+num1=int(input("Digite el primer numero: "))
+num2=int(input("Digite el segundo numero: "))
+num3=int(input("Digite el tercer numero: "))
+
+if (num1 > num2 and num1 > num3):
+    print(f"El numero mayor es: {num1}") 
+elif (num2 > num1 and num2 > num3):
+    print(f"El numero mayor es: {num2}") 
+elif (num3 > num1 and num3 > num2):
+    print(f"El numero mayor es: {num3}") 
