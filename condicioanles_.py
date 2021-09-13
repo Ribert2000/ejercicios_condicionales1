63bb606e130ca07b7963bdaabc7077ba2d58186e
Ribert2000 <ribert_rodriguez21@hotmail.com>
2021-09-13 12:10:23 -0500

    segundo commit

--- condicioanles_.py
+++ condicioanles_.py
@@ -4,4 +4,11 @@ Created on Mon Sep 13 12:04:20 2021
 
 @author: riber
 """
-
+#ej1
+total = int(input("¿Cuantas camisas usted va a llevar?: "))
+if (total <= 0):
+    print("Digite una cantidad valida!")
+elif (total <= 2):
+    print("Usted tendra un descuento del 10% amigo!")
+else:
+    print("Usted tendra un descuento del 30% en el total de su compra")
